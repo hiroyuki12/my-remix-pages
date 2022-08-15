f59200b7d7c03df998dd95ecde6673e4ea0c19f4
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-15 10:37:36 +0900

    Add ServiceWorker(PWA).

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,3 +2,9 @@ import { RemixBrowser } from "@remix-run/react";
 import { hydrate } from "react-dom";
 
 hydrate(<RemixBrowser />, document);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
